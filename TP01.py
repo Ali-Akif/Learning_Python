e38a8590f1cc4ffb74618d3f988a1f1762757a62
Ali-Akif <ali-akif@live.fr>
2023-12-15 08:07:40 +0100

    Update TP01.py
    pstpstpst

--- TP01.py
+++ TP01.py
@@ -13,12 +13,12 @@ else:
 #In correction, he used a boolean var bissextile assigned with false and worked with it ( if biss % 400 == 0 then its true ) and printed at the end if bissextile print it is else print it's not
 
 
+#bonus
+
 content = input("T'es content frérot ? Réponds en binaire bg\n")
 content = int(content)  
 
 if content :
    print("hiiiiii il est content")
 else:
-   print("tocard")      
-    
-#bonus
+   print("tocard")
