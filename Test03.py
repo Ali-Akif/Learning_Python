a8a4acb8300d75b778473d90d8c6df9aeb850c33
Ali-Akif <ali-akif@live.fr>
2023-12-15 20:43:28 +0100

    Create Test03.py

---
+++ Test03.py
@@ -0,0 +1,12 @@
+#learning how to create a function
+
+def table(nb, max=10):
+    """
+erjehhehehe 
+    """
+    i = 0
+    while i < max:
+        i += 1
+        print(i, "*", nb, "=", i * nb)
+
+table(8)
