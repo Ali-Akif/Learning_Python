2afc9fbc745c580e711b26ccb8c14aa1e453bf51
Ali-Akif <ali-akif@live.fr>
2023-12-17 21:51:21 +0100

    Update 05.py

--- PT2.POO/05.py
+++ PT2.POO/05.py
@@ -4,6 +4,12 @@ print(chaine[4])
 print(chaine[-1]) #sors le dernier caractere
 print(chaine[-4]) #l'indexation se fait a partir de -1 de droite a gauche et de 0 de gauche a droite
 
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+index_n = alphabet.index("n")
+
+print(alphabet[index_n:])
+
 
 chaine = "Salut"
 print(len(chaine)) #len() pour sortir le nombre de caractere
