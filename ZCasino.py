9ecbde2b483215b0a7e885732b5fc74f86d867e6
Ali-Akif <ali-akif@live.fr>
2023-12-17 15:51:28 +0100

    Update ZCasino.py
    fixed bug

--- ZCasino.py
+++ ZCasino.py
@@ -6,57 +6,62 @@ from math import ceil
 argent = 1000
 jeu = True
 
-print("Tu as", argent,"$.")
-
-
+print("Tu commences avec", argent,"$.")
 
 while jeu:
-    
-
-    try:
-        n = int(input("Mise sur un chiffre entre 0 et 49 frérot :"))
-    except ValueError:
-        print("Faut rentrer un chiffre frérot...")
-        continue
-    if n < 0 or n > 49:
-        print("Apprends à lire frère.")
-    
-    mise = 0
-
-    while mise <= 0 or mise > argent:
+    n = -1 #sinon on sors jamais de la boucle try
+    while n == -1:
         try:
-            mise = int(input("Tu veux miser combien ?"))
+            n = int(input("Sur quelle chiffre tu veux miser ?"))
         except ValueError:
-            print("Faut rentrer un chiffre.")
+            print("Rentre un chiffre gros...")
             continue
-        if mise > argent:
-            print("T'as pas cette somme, t'as", argent,"$.")
-        if mise <= 0:
-            print("Faut miser un peu quand meme...")
-    
+        if n < 0 or n > 49:
+            print("Faut rentrer un chiffre entre 0 et 49...")
+            
+    if 0 <= n <= 49: #Ne doit surtout pas etre placé au meme niveau que try, sinon jeu passera direct a la suite dans rentrer de mise, et la c'est la cata frérot, cela dit il doit etre placé au meme niveau que while, pour que while puisse le prendre en compte 
+        mise = 0
+        while mise <= 0:
+            try:
+                mise = int(input("Tu veux miser combien ? "))
+            except ValueError:
+                print("Rentre un chiffre gros...")
+                continue
+            if mise <= 0:
+                print("Faut miser un peu quand meme !")
+            elif mise > argent:
+                print("T'as ça toi ? Tu as", argent,"$ je te rappelle !")
+
     n_gagnant = randrange(50)
+    print("La roue tourne...")
+    print("C'est le numéro", n_gagnant,"!")
 
     if n == n_gagnant:
-        print("Bravo, tu gagnes", mise * 3,"$!!!")
-        argent += mise*3
+        mise += mise * 3
+        print("Bravo, t'as gagné", mise,"$!!!")
+        argent += mise
     elif n % 2 == n_gagnant % 2:
-        print("Même couleur, tu gagnes la moitié de ta mise, soit", mise * 0.5, "$.")
         mise = ceil(mise * 0.5)
+        print("Bonne couleur, t'as gagné", mise,"$!")
         argent += mise
     else:
-        print("Loupé, tu perds ta mise de", mise,"$...")
-        argent -= mise  
-    
-    if argent <= 0:
-        print("T'es ruiné, j'tai bien niqué chacal, finito pour toi la partie.")
-        jeu = False
-    else:
-        print("Tu as", argent,"$.")
-        quitter = input (" Veux tu quitter le casino (o/n) ? ")
-        if quitter == "o" or quitter == "O":
-            print("Vous quittez le casino avec vos gains.")
-            jeu = False
+        print("Pas de chance, tu as perdu", mise,"$...")
+        argent -= mise
 
-input("Appuyez sur Entrée pour quitter...")
+    if argent <= 0:
+        jeu = False
+        print("t'as pu de thune")
     
-   
+    if argent > 0:
+        print("Tu as désormais", argent,"$.")
+        continuer_jeu = input("Tu veux continuer la partie ? (o/n)")
+    if continuer_jeu == "n" or continuer_jeu == "N":
+        jeu = False
+        print("Bye bye...")
+
+
+input("Appuie sur entrée pour quitter...")
+    
+
+        
+        
