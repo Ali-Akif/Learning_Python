38ff58f906f23b76440e4eaf4faedb2c7d287557
Ali-Akif <ali-akif@live.fr>
2023-12-17 14:14:14 +0100

    Update ZCasino.py

--- ZCasino.py
+++ ZCasino.py
@@ -6,16 +6,29 @@ from math import ceil
 argent = 1000
 jeu = True
 
-print("Vous avez", argent,"$.")
+print("Salut mon reuf, tu commences avec", argent,"$.")
 
 while jeu:
-    n = -1
+
     try:
-        n = int(input("Sur quelle chiffre voulez vous miser :"))
+        n = int(input("Mise sur un chiffre entre 0 et 49 frérot :"))
     except ValueError:
-        print("Il faut miser sur un CHIFFRE.")
+        print("Faut rentrer un chiffre frérot...")
         continue
     if n < 0 or n > 49:
-        print("Il faut rentrer un chiffre entre 0 et 49 l'ami.")
-            
+        print("Apprends à lire frère.")
+    
+    mise = 0
 
+    while mise <= 0 or mise > argent:
+        try:
+            mise = int(input("Tu veux miser combien ?"))
+        except ValueError:
+            print("Faut rentrer un chiffre.")
+            continue
+        if mise > argent:
+            print("T'as pas cette somme, t'as", argent,"$.")
+        if mise <= 0:
+            print("Faut miser un peu quand meme...")
+    
+   
