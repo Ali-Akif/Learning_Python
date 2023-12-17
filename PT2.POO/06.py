e6fb3292c0bebb49f332952788c375fb2c48ef34
Ali-Akif <ali-akif@live.fr>
2023-12-17 20:03:27 +0100

    Create 06.py

---
+++ PT2.POO/06.py
@@ -0,0 +1,24 @@
+liste = ()
+print(type(liste)) # <class 'tuple'>
+
+print(liste) #print ()
+
+liste = []
+print(liste) #print []
+
+liste = [1,2,3,4,5,6,7,8,9,10]
+
+print(liste) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+liste =[1,"chaine",2]
+
+print(liste) #[1, 'chaine', 2]
+
+print(liste[0]) #1
+
+
+
+liste.append("neeew")
+
+print(liste) #ajoute neeew a la fin de la liste
+ 
