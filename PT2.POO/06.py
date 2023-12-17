416afafdc772f762120215027563a8459f586d45
Ali-Akif <ali-akif@live.fr>
2023-12-17 20:15:00 +0100

    Update 06.py

--- PT2.POO/06.py
+++ PT2.POO/06.py
@@ -1,10 +1,10 @@
 liste = ()
 print(type(liste)) # <class 'tuple'>
 
-print(liste) #print ()
+print(liste) #print (), () est de la classe tuple, ils sont immuables
 
 liste = []
-print(liste) #print []
+print(liste) #print [], [] peut etre modifié
 
 liste = [1,2,3,4,5,6,7,8,9,10]
 
