88d722a191649300c761b5e0c25da70864f99eea
Ali-Akif <ali-akif@live.fr>
2023-12-17 19:26:45 +0100

    Create 03.py

---
+++ PT2.POO/03.py
@@ -0,0 +1,13 @@
+minuscules = "une chaine en minuscules"
+minuscules = minuscules.upper()
+print(minuscules) #sors minuscules écrit en maj
+minuscules = minuscules.capitalize()
+print(minuscules) #Premiere lettre en maj
+
+espaces = " Voila pleins d'e s p a c e u h "
+espaces = espaces.strip()
+print(espaces) #retire espace du début et fin de chaine
+
+titre = "introduction"
+titre = titre.upper().center(20)
+print(titre)#center ajoute la longueur a celle demandé, en centrant str
