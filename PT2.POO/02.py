1b632737fc3aef5d321c0972a3e08aa1637cf16d
Ali-Akif <ali-akif@live.fr>
2023-12-17 19:16:24 +0100

    Create 02.py

---
+++ PT2.POO/02.py
@@ -0,0 +1,9 @@
+chaine = "" 
+while chaine.lower() != "q":
+    print("Tapez q pour quitter...")
+    chaine = input()
+#tant que l'utilisateur rentre pas q ou Q, chaine ne correspond pas a q, donc forcément il va pas quitter la boucle, j'ai buggé sur ça mdr
+
+print("Merci !")
+
+#si on imprime chaine, ca sera q ou Q, parce que pour en arriver la, il a fallu que q soit égal a q, logique héhéhéééé..... j'suis teubé
