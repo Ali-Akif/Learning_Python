19d331acdab5fbd53130634c2b70e13b5e61e0c5
Ali-Akif <ali-akif@live.fr>
2023-12-18 09:05:27 +0100

    Update 11.py

--- PT2.POO/11.py
+++ PT2.POO/11.py
@@ -12,3 +12,13 @@ fonction_inconnue('a', 'b', 2)
 
 var = 3.5
 fonction_inconnue(var, [4], "...")  
+
+for i in range(5):
+    print (i)
+
+    # print de 0 a 4
+
+for i in range(1,5):
+    print (i)
+
+    #print de 1 a 4
