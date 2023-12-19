a104d7239132dd7fa59384f14c2ba0935b82aa33
Ali-Akif <ali-akif@live.fr>
2023-12-19 13:34:49 +0100

    Create test.py

---
+++ PT2.POO/test.py
@@ -0,0 +1,22 @@
+
+import sys
+
+for i in sys.argv[1]:
+    print(i)
+
+#argument lslsl et ca print  : l s l s l avec un retour a la ligne a chaque fois
+    
+for i in sys.argv[1:]:
+    print(i)
+
+#print lslsl
+    
+print(sys.argv[1])
+
+#print lslsl
+
+print(sys.argv[1:])
+#print ['lslsl']
+
+print(sys.argv) 
+#print ['test.py', 'lslsl'] 
