68a27a2e58b9fd94fabbeccdc9771a5936bd8555
Ali-Akif <ali-akif@live.fr>
2024-01-24 21:47:10 +0100

    Update set.py

--- Others/set.py
+++ Others/set.py
@@ -18,4 +18,3 @@ print(a | b) #{1, 2, 3, 4, 5}, union
 print(a & b) #{3}, intersection
 print(a - b) #{1, 2}, difference, sort l'élément present dans a mais pas dans b
 print(a ^ b) #{1, 2, 4, 5}, difference symetrique, ne montre pas les doublons de a et b
-
